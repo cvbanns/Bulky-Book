d7dba191d1626f989bac69228e48848ea3a2a59f
cvbanns <calebaregbesola@gmail.com>
2023-02-12 23:44:21 +0100

    Edit feature for Category razor page

--- BulkyBook/BulkyBookWeb/Models/Category.cs
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -7,7 +8,9 @@ namespace BulkyBookWeb.Models
         [Key]
         public int Id { get; set; }         
         [Required]
-        public string Name { get; set; }    
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1 and 100 only")]
         public int DisplayOrder { get; set; } 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
     }
