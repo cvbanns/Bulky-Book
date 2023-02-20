a49763e8bb43f396b8a192685d21c4616f7d35ee
cvbanns <calebaregbesola@gmail.com>
2023-02-20 02:26:40 +0100

    Ft-Upsert-Product-Entity-Using-ViewModel

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var productFromDb = _db.Products.FirstOrDefault(p => p.Id == product.Id);
+            if (productFromDb == null)
+            {
+                productFromDb.Title = product.Title;
+                productFromDb.ISBN = product.ISBN;
+                productFromDb.Price= product.Price;
+                productFromDb.ListPrice= product.ListPrice;
+                productFromDb.Price50= product.Price50;
+                productFromDb.Price100= product.Price100;
+                productFromDb.Descripton= product.Descripton;
+                productFromDb.CategoryId= product.CategoryId;
+                productFromDb.Author= product.Author;
+                productFromDb.CoverTypeId= product.CoverTypeId;
+
+                if(product.ImageUrl!= null)
+                {
+                    productFromDb.ImageUrl= product.ImageUrl;
+                }
+            }
+        }
+    }
+}
---
+++ BulkyBook/BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> coverTypes = _unitOfWork.CoverType.GetAll();
+            return View(coverTypes);
+        }
+        
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+
+            };
+            if (id == null || id == 0)
+            {
+                //Create product
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
+            }
+            else
+            {
+                //Update Product
+            }
+            
+            return View(productVM);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType coverType)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(coverType);
+                _unitOfWork.Save();
+                TempData["success"] = "Cover Type updated successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View(coverType);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var coverType = _unitOfWork.CoverType.GetFirstOrDefault(x => x.Id == id);
+            if (coverType == null)
+            {
+                return NotFound();
+            }
+            return View(coverType);
+        }
+
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var coverType = _unitOfWork.CoverType.GetFirstOrDefault(x => x.Id == id);
+            if (coverType == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.CoverType.Remove(coverType);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover Type deleted successfully";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
